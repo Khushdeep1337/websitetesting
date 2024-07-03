93328d7cc0f342b284dc187e7592d373630e0306
Khushdeep1337 <Khushdeep2004@gmail.com>
2024-07-03 11:57:39 -0600

    revert

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-window.onload = function() {
-    var contact = document.getElementById('contactBtn');
-
-    contact.addEventListener('click', function() {
-        window.location.href = 'contact-form.html';
-    });
+function myFunction() {
+    alert("Hello, World!");
 }
